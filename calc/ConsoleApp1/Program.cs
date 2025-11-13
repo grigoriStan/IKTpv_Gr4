3650a2ce30b56f7aa62627239e41f1cd0910ccbd
Opilane TTHK <opilane@tthk.ee>
2025-11-13 10:45:04 +0200

    calc

---
+++ calc/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            float firstNumber = float.Parse(Console.ReadLine());
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            
+
+            string imput = Console.ReadLine();
+            switch (imput)
+            {
+                case "+":
+                    Console.WriteLine($"tulemus: {firstNumber + secondNumber}");
+                        break;
+                case "-":
+                    Console.WriteLine($"tulemus: {firstNumber - secondNumber}");
+                        break;
+                case "*":
+                    Console.WriteLine($"tulemus: {firstNumber * secondNumber}");
+                        break;
+                case "/":
+                    Console.WriteLine($"tulemus: {firstNumber / secondNumber}");
+                        break;
+
+
+
+
+            }
+        }
+    }
+}
